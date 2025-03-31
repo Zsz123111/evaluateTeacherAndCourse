4ffc27e5d8334e0e4840396e3b503ea9c80e8d6f
Shizhao_Cheung <910678508@qq.com>
2025-03-31 22:59:42 +0800

    update '针对版本更新导致的文字框无法填充进行了修复。'

--- 自动填写教师评估表单.user.js
+++ 自动填写教师评估表单.user.js
@@ -17,8 +17,10 @@
     });
 
     // 填写主观题的默认答案
-    document.querySelector('textarea[name="item_1403"]').value = "老师教学内容贯通、严谨、科学，学习气氛活跃。";
-    document.querySelector('textarea[name="item_1404"]').value = "希望增加课后作业环节，进一步巩固学习内容。";
+    // 这里的item_****可能会有变化，导致文字部分无法自动填写，具体操作就是在浏览器中
+    // 鼠标右键，点击检查，查看开发者模式下网页源码对应部分的item代码，然后对应文字框修改为对应编码即可
+    document.querySelector('textarea[name="item_1431"]').value = "老师教学内容贯通、严谨、科学，学习气氛活跃。";
+    document.querySelector('textarea[name="item_1432"]').value = "希望增加课后作业环节，进一步巩固学习内容。";
 
     // 检查并填写验证码（请手动输入验证码）
     document.querySelector('input[name="adminValidateCode"]').focus();
